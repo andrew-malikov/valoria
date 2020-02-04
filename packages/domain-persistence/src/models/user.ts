1179880124d558e7703306cb8a25df3121960173
markupcode <andrew.devels@gmail.com>
2020-02-04 23:51:05 +0300

    feat(domain/persistence): store salt to encrypt/decrypt valuable user data

--- packages/domain-persistence/src/models/user.ts
+++ packages/domain-persistence/src/models/user.ts
@@ -1,5 +1,9 @@
+/**
+ * @property {string} salt to encrypt user data
+ */
 export interface User {
     readonly profile: { name: string };
+    readonly salt: string;
 }
 
 export const UserStaticKind = 'user';
